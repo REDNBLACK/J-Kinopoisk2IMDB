de140d605e907c9373a10aa4ae74b031fc59d40e
REDNBLACK <rednblack@protonmail.com>
2016-04-08 04:21:26 +0300

    Made EqualsTitleComparator final, added core package version

--- core/src/main/java/org/f0w/k2i/core/comparator/title/EqualsTitleComparator.java
+++ core/src/main/java/org/f0w/k2i/core/comparator/title/EqualsTitleComparator.java
@@ -3,7 +3,7 @@ package org.f0w.k2i.core.comparator.title;
 import org.f0w.k2i.core.comparator.AbstractMovieComparator;
 import org.f0w.k2i.core.model.entity.Movie;
 
-public class EqualsTitleComparator extends AbstractMovieComparator {
+public final class EqualsTitleComparator extends AbstractMovieComparator {
     @Override
     public boolean areEqual(Movie movie1, Movie movie2) {
         String title1 = movie1.getTitle();
